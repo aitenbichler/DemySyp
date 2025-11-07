348c0702ef1745d542556a971123933d5019514f
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2025-11-07 11:30:33 +0100

    cmd file

--- LeoCloud/UpdateLeocloudFrontend.cmd
+++ LeoCloud/UpdateLeocloudFrontend.cmd
@@ -1,9 +1,10 @@
-kubectl delete -n student-h-aitenbichler deployment demosypui
-kubectl delete -n student-h-aitenbichler service demosypui-svc
-kubectl delete -n student-h-aitenbichler ingress demosypui-ingress
-kubectl delete -n student-h-aitenbichler pod -l app=demosypui
-kubectl create -f leocloud-deploy-frontend.yaml
+set NameSpace=student-h-aitenbichler
+set AppPrefix=demosypui
 
+kubectl delete -n %NameSpace% deployment %AppPrefix%
+kubectl delete -n %NameSpace% service %AppPrefix%-svc
+kubectl delete -n %NameSpace% ingress %AppPrefix%-ingress
+kubectl delete -n %NameSpace% pod -l app=%AppPrefix%
+kubectl create -f ./frontend-leocloud-%NameSpace%-%AppPrefix%.yaml
 
 pause
-:: kubectl apply -f leocloud-deploy-V2.yaml
